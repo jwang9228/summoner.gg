8ec5b2ffe423f7417cf87b3b22eee18195c20bda
jwang9228 <wang.justi@northeastern.edu>
2023-11-14 16:26:13 -0500

    Styles and components for initial homepage

--- src/summoner-app/index.js
+++ src/summoner-app/index.js
@@ -1,15 +1,34 @@
 import './home.css';
 import '../common/colors.css';
+import React from 'react';
+import { Form, Button, InputGroup } from 'react-bootstrap';
+import { CgSearch } from 'react-icons/cg';
 
 function SummonerApp() {
-    return (
-        <div>
-            <p className='app-title'>
-                SUMMONER
-                <span className='app-blue-accent'>.</span>
-                GG
-            </p>
-        </div>
-    );
+	return (
+		<div>
+			<p className='app-title mb-3 mt-3'>
+				SUMMONER
+				<span className='app-blue-accent'>.</span>
+				GG
+			</p>
+			<InputGroup className='m-auto w-50' size='lg'>
+                <InputGroup.Text className='white-background'>
+                    <Button className='region-button app-blue-accent' size='sm'>
+                        NA
+                    </Button>
+                </InputGroup.Text>
+				<Form.Control
+                    placeholder='Search for Summoner'
+                    className='border-0 search-bar'
+				/>
+                <InputGroup.Text className='white-background'>
+                    <Button className='white-background search-button'>
+                        <CgSearch className='search-icon' size={24}/>
+                    </Button>
+                </InputGroup.Text>
+			</InputGroup>
+		</div>
+	);
 }
 export default SummonerApp;
